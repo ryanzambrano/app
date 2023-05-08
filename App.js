6f7f3a669764c25228359458bfe8ed4fff67c230
ryanzambrano <rez1237@gmail.com>
2023-05-08 14:40:53 -0400

    Initial commit

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style = { styles.mid }>Start matching with roomates!!</Text>
+      <Button title="Sign Up"> Details </Button>
+      <View style = {styles.box}></View>
+      <View style = {styles.box}></View>
+      <View style = {styles.box}></View>
+      <View style = {styles.balls}>
+        <View style = {styles.box}></View>
+        <View style = {styles.box}></View>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +23,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
+
+  mid: {
+    fontSize: 50,
+  },
+
+  box: {
+    width: 100, height: 100, backgroundColor: 'green', borderRadius: 1,
+  },
+
+  balls: {
+    flex: 1,
+    flexDirection: 'row',
+  }
+
+
 });
