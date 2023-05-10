d192b4af55e72f7f87ebf954aae834c5e63e2774
mattbrwnn <mattb7854@icloud.com>
2023-05-10 12:42:19 -0400

    Put in email and password entry + sign in button + sign up button with non autocorrecting email keyboard and hidden asterisk password entry

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
-import React from 'react-native';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View, Image, SafeAreaView } from 'react-native';
-import { Button } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Image, SafeAreaView, TouchableOpacity, Button } from 'react-native';
 
 export default function App() {
+
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  });
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: '#eBecf4' }}>
       <View style={styles.container}>
@@ -16,10 +21,58 @@ export default function App() {
           <Text style = { styles.titleText }>Sign into RoomBa</Text>
           <Text style = { styles.sloganText }>Find and meet new roomates, for any situation!</Text>
         </View>
-        
 
+        <View style={styles.form}>
+
+          <View style={styles.input}>
+            <Text style={styles.inputHeader}>Email Address:</Text>
+            <TextInput style={styles.inputControl} 
+              autoCapitalize='none'
+              autoCorrect={false}
+              keyboardType='email-address'
+              placeholder='deeznuts@bruh.com'S
+              placeholderTextColor="#6b7280"
+              value={form.email} 
+              onChangeText={email => setForm({ ...form, email })} 
+            />
+          </View>
+
+          <View style={styles.input}>
+            <Text style={styles.inputHeader}>Password:</Text>
+            <TextInput style={styles.inputControl} 
+              styles={styles.inputControl}
+              autoCorrect={false}
+              placeholder='*********'
+              placeholderTextColor="#6b7280"
+              value={form.password} 
+              onChangeText={password => setForm({ ...form, password })}
+              secureTextEntry={true}
+            />
+          </View>
+
+          <View style={styles.formAction}>
+            <TouchableOpacity
+              onPress={() => {
+                // handle onPress
+              }}>
+              <View style={styles.continue}>
+                <Text style={styles.continueText}>Sign in</Text>
+              
+              </View>
+            </TouchableOpacity>
+          </View>
+
+          <TouchableOpacity
+            onPress={() => {
+              // handle link
+            }}
+            style={{ marginTop: 'auto' }}>
+            <Text style={styles.formFooter}>
+              Don't have an account?{' '} <Text style={{ textDecorationLine: 'underline' }}>Sign up</Text>
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
-
     </SafeAreaView>
 
     
@@ -30,9 +83,6 @@ const styles = StyleSheet.create({
   container: {
     padding: 24,
     flex: 1,
-    // backgroundColor: 'white',
-    // alignItems: 'center',
-    // justifyContent: 'center',
   },
 
   header: {
@@ -51,7 +101,7 @@ const styles = StyleSheet.create({
     fontSize: 27,
     fontWeight: '700',
     textAlign: "center",
-    marginBottom: 8,
+    marginBottom: 12,
     color: '#1e1e1e',
   },
 
@@ -63,12 +113,69 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
 
+  input: {
+    marginBottom: 16,
+  },
 
-  
+  inputHeader: {
+    fontSize: 17,
+    fontWeight: '500',
+    color: '#222',
+    marginBottom: 10,
+  },
+
+  inputControl: {
+    backgroundColor: '#fff',
+    color: '#222',
+    fontWeight: '500',
+    height: 44,
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+    borderRadius: 12,
+    fontSize: 15,
+  },
+
+  form: {
+    marginBottom: 24,
+    flex: 1,
+  },
+
+  formAction: {
+    marginVertical: 24,
+  },
+
+  formFooter: {
+     fontSize: 17,
+     fontWeight: '600',
+     color: '#222',
+     textAlign: 'center',
+     letterSpacing: 0.15,
+  },
+
+  continue: {
+    marginTop: 5,
+    marginBottom: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 8,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderWidth: 1,
+    backgroundColor: '#075eec',
+    borderColor: '#075eec',
+  },
+
+  continueText: {
+    fontSize: 18,
+    lineHeight: 26,
+    fontWeight: '600',
+    color: '#fff',
+  },
 
   none: {
     
-  }
+  },
 
 
 });
