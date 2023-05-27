92eb30cd45ecc0fbcb8e7d8da94c6debdf150430
Mason Hall <66487382+halmas-sudo@users.noreply.github.com>
2023-05-27 18:08:55 -0400

    Profile and Arrow and Bubble Everything

--- components/messages.js
+++ components/messages.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, FlatList, StyleSheet, Text, KeyboardAvoidingView, Platform } from 'react-native';
+import { View, TextInput, Button, FlatList, StyleSheet, Text, KeyboardAvoidingView, Platform, TouchableOpacity } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 
 const MessagingUI = () => {
   const [message, setMessage] = useState('');
@@ -7,7 +8,7 @@ const MessagingUI = () => {
 
   const sendMessage = () => {
     if (message.trim() !== '') {
-      setMessages(prevMessages => [...prevMessages, message]);
+      setMessages(prevMessages => [message, ...prevMessages]); // Prepend the new message to the messages array
       setMessage('');
     }
   };
@@ -16,14 +17,28 @@ const MessagingUI = () => {
     <KeyboardAvoidingView
       style={styles.container}
       behavior={Platform.OS === 'ios' ? 'padding' : null}
-      keyboardVerticalOffset={Platform.OS === 'ios' ? 80 : 0}
+      keyboardVerticalOffset={Platform.OS === 'ios' ? 90 : 0}
     >
+      <View style={styles.header}>
+        <View style={styles.profileContainer}>
+          {/* Replace the placeholder with your circular profile picture component */}
+          <View style={styles.profilePicture} />
+        </View>
+        <TouchableOpacity style={styles.button}>
+          <AntDesign name="arrowright" size={24} color="#007AFF" />
+        </TouchableOpacity>
+      </View>
       <View style={styles.messagesContainer}>
         <FlatList
           data={messages}
-          renderItem={({ item }) => <Text style={styles.message}>{item}</Text>}
+          renderItem={({ item }) => (
+            <View style={styles.messageContainer}>
+              <Text style={styles.message}>{item}</Text>
+            </View>
+          )}
           keyExtractor={(_, index) => index.toString()}
           contentContainerStyle={styles.messagesContent}
+          inverted // Reverse the order of messages to display the most recent at the bottom
         />
       </View>
       <View style={styles.inputContainer}>
@@ -45,6 +60,29 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     backgroundColor: '#F9F9F9',
+    marginBottom: 10,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  button: {
+    padding: 10,
+  },
+  profileContainer: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: '#dedede',
+    overflow: 'hidden',
+    marginRight: 10,
+  },
+  profilePicture: {
+    flex: 1,
+    backgroundColor: 'lightgray',
   },
   messagesContainer: {
     flex: 1,
@@ -53,14 +91,17 @@ const styles = StyleSheet.create({
     paddingTop: 10,
     paddingBottom: 20,
   },
-  message: {
-    fontSize: 16,
-    paddingVertical: 5,
-    paddingHorizontal: 10,
-    backgroundColor: '#dedede',
-    borderRadius: 10,
+  messageContainer: {
+    borderRadius: 20,
     marginBottom: 10,
     alignSelf: 'flex-start',
+    backgroundColor: '#dedede',
+  },
+  message: {
+    fontSize: 16,
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+    color: '#000',
   },
   inputContainer: {
     flexDirection: 'row',
@@ -76,6 +117,9 @@ const styles = StyleSheet.create({
     height: 40,
     marginRight: 10,
     color: '#333',
+    borderRadius: 20,
+    paddingHorizontal: 10,
+    backgroundColor: '#FFF',
   },
 });
 
