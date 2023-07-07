ab3929fce4b7e2be04ce90f8c1c9748bba1ae7a5
Mason Hall <66487382+halmas-sudo@users.noreply.github.com>
2023-07-07 18:58:27 -0400

    Thee Merge Finale
    XD RAWR UWU :3

--- components/ThreeMainPages.js
+++ components/ThreeMainPages.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import MessagingUI from './messages';
 import TabNavigator from './TabNavigator';
+import ProfileUI from './otherprofile';
 
 const Stack = createStackNavigator();
 
@@ -13,6 +14,7 @@ const ThreeMainPages = () => {
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Tabs" component={TabNavigator} />
         <Stack.Screen name="Message" component={MessagingUI} />
+        <Stack.Screen name="OtherProfile" component={ProfileUI} />
       </Stack.Navigator>
     </NavigationContainer>
   );
