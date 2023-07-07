355b6f49e6c62df3a7f3fa8cfdb963bfd0a8835a
Mason Hall <66487382+halmas-sudo@users.noreply.github.com>
2023-07-07 18:36:20 -0400

    WeGottemBoys
    Finally

--- components/ThreeMainPages.js
+++ components/ThreeMainPages.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
+import MessagingUI from './messages';
 import TabNavigator from './TabNavigator';
 
+const Stack = createStackNavigator();
+
 const ThreeMainPages = () => {
-  
   return (
     <NavigationContainer independent={true}>
-      <TabNavigator/>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Tabs" component={TabNavigator} />
+        <Stack.Screen name="Message" component={MessagingUI} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
